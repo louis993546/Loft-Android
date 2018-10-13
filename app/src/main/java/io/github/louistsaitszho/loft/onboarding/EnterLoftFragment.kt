eeafe2c19f830e40cf80e51dbd9a78f7ec4d003f
Louis Tsai <louis993546@gmail.com>
2018-10-14 00:12:41 +0200

    Show splash screen; navigation basics; simplify resources

---
+++ app/src/main/java/io/github/louistsaitszho/loft/onboarding/EnterLoftFragment.kt
@@ -0,0 +1,15 @@
+package io.github.louistsaitszho.loft.onboarding
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.louistsaitszho.loft.R
+
+class EnterLoftFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_enter_loft, container, false)
+    }
+}
--- app/src/main/java/io/github/louistsaitszho/loft/splash/SplashFragment.kt
+++ app/src/main/java/io/github/louistsaitszho/loft/splash/SplashFragment.kt
@@ -7,11 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 
 import io.github.louistsaitszho.loft.R
+import timber.log.Timber
 
 class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("splash resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("splash paused")
     }
 }
