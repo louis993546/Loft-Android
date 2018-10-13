850ee1b5dbb0886e3da2d13f29b6b47b8c014867
Louis Tsai <louis993546@gmail.com>
2018-10-14 00:26:11 +0200

    Prevent user "back" back to splash

--- app/src/main/java/io/github/louistsaitszho/loft/splash/SplashFragment.kt
+++ app/src/main/java/io/github/louistsaitszho/loft/splash/SplashFragment.kt
@@ -1,13 +1,13 @@
 package io.github.louistsaitszho.loft.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.navigation.fragment.findNavController
 import io.github.louistsaitszho.loft.R
-import timber.log.Timber
 
 class SplashFragment : Fragment() {
 
@@ -16,13 +16,11 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
-        Timber.d("splash resume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Timber.d("splash paused")
+    override fun onStart() {
+        super.onStart()
+        val delayHandler = Handler()
+        delayHandler.postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_whatIsLoftFragment)
+        }, 2000)
     }
 }
