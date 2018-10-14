2897b4c627f41f2c12222ef074bb4b6588fdc0f8
Louis Tsai <louis993546@gmail.com>
2018-10-14 03:20:14 +0200

    Add layout for WhatIsLoft; fix color names

--- app/src/main/java/io/github/louistsaitszho/loft/onboarding/WhatIsLoftFragment.kt
+++ app/src/main/java/io/github/louistsaitszho/loft/onboarding/WhatIsLoftFragment.kt
@@ -5,12 +5,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import io.github.louistsaitszho.loft.R
+import kotlinx.android.synthetic.main.fragment_what_is_loft.*
 
 class WhatIsLoftFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_what_is_loft, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fab_next.setOnClickListener {
+            findNavController().navigate(R.id.action_whatIsLoftFragment_to_enterLoftFragment)
+        }
     }
 }
