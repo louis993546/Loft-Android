1d436451b290d1f92f4edce57722fd79b25dd461
Louis Tsai <louis993546@gmail.com>
2018-11-10 10:22:49 +0100

    #26: Change project level gradle script to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlin_version = "1.3.0"
+    val support_library_version = "27.1.1"
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+    val compile_sdk_version by extra { 28 }
+    val min_sdk_verion by extra { 21 }
+    val target_sdk_version by extra { 27 }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+include(
+        ":app",
+        ":common",
+        ":api",
+        ":onboarding"
+)
