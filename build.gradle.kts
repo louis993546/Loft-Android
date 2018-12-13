88996308ea747f4e50001e90ac5b7733022ecb68
Louis Tsai <louis993546@gmail.com>
2018-12-13 21:44:54 +0100

    FIX: gradle to make it work in AS 3.4 alpha 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-alpha07")
+        classpath("com.android.tools.build:gradle:3.4.0-alpha08")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
